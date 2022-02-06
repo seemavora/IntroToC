3f7bfa1e4473d82093063b05c4e21b3181d660a8
seemavora <seema.s.vora@gmail.com>
2022-02-05 21:18:39 -0800

    Adds Simple Code to Takin in Command Line Arguments

---
+++ Chapter2/takingInput.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+// very simple code that will take in a parameter for the amount of arguments that exist
+
+int main( int argc, char *argv[] )  {
+
+   printf("Hello World - %d\n", argc); // prints the number of arguments
+   // by default there is one arg which is the name of the file
+}
